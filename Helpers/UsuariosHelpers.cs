6168a47ef56bf650da4986c7e5afa0cac9ce1fd7
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-14 10:26:53 -0300

    Remoção lixo

--- Helpers/UsuariosHelpers.cs
+++ Helpers/UsuariosHelpers.cs
@@ -9,40 +9,6 @@ public static class UsuariosHelpers
 {
     public static int ObterIdUsuario(HttpRequest request)
     {
-
-        #region Implementação antiga
-        //var token = request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-        //var tokenHandler = new JwtSecurityTokenHandler();
-        //var key = Encoding.ASCII.GetBytes(_config["Jwt:key"]);
-        //var tokenValidationParameters = new TokenValidationParameters
-        //{
-        //    ValidateIssuerSigningKey = true,
-        //    IssuerSigningKey = new SymmetricSecurityKey(key),
-        //    ValidateIssuer = true,
-        //    ValidIssuer = _config["TokenConfiguration:Issuer"],
-        //    ValidateAudience = true,
-        //    ValidAudience = _config["TokenConfiguration:Audience"],
-        //    ValidateLifetime = true,
-        //    ClockSkew = TimeSpan.Zero
-        //};
-
-        //SecurityToken validatedToken;
-        //var claimsPrincipal = tokenHandler.ValidateToken(token, tokenValidationParameters, out validatedToken);
-
-        //var idUsuarioClaim = claimsPrincipal.Claims.FirstOrDefault(claim => claim.Type == "idUsuario");
-        //if (idUsuarioClaim == null)
-        //{
-        //    throw new Exception("Token inválido: idUsuario não encontrado");
-        //}
-
-        //if (!int.TryParse(idUsuarioClaim.Value, out int idUsuario))
-        //{
-        //    throw new Exception("Token inválido: idUsuario inválido");
-        //}
-
-        //return idUsuario;
-        #endregion
-
         StringValues authHeader;
         if (request.Headers.TryGetValue("Authorization", out authHeader) &&
             authHeader.FirstOrDefault()?.StartsWith("Bearer ") == true)
