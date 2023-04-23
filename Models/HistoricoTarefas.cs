4dd601432055a7c5df5335f7194431e35d3341ae
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-23 10:50:34 -0300

    alteracoes

--- Models/HistoricoTarefas.cs
+++ Models/HistoricoTarefas.cs
@@ -1,11 +1,10 @@
-﻿namespace WebApi.Models
+﻿namespace WebApi.Models;
+
+public class HistoricoTarefas 
 {
-    public class HistoricoTarefas 
-    {
-        public int IdHistoricoTarefa { get; set; }
-        public int IdTarefa { get; set; }
-        public DateTime DataAlteracao { get; set; }
-        public string? DescricaoAlteracao { get; set; }
-        public virtual Tarefas Tarefa { get; set; }
-    }
+    public int IdHistoricoTarefa { get; set; }
+    public int IdTarefa { get; set; }
+    public DateTime DataAlteracao { get; set; }
+    public string? DescricaoAlteracao { get; set; }
+    public virtual Tarefas Tarefa { get; set; }
 }
