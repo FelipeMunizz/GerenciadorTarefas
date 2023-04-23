ade303f377bcb83596f4650facb99397a3d961e1
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-23 12:27:58 -0300

    adição do banco do not

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,8 @@ public class AppDbContext
 {
     public static string GetConnectionString()
     {
-        return "Data Source=DESKTOP-10DDISU;Initial Catalog=GerenciadorTarefas;Integrated Security=True;Pooling=False;Encrypt=False;TrustServerCertificate=False;";
+        return "Data Source=DESKTOP-V672319\\SQLEXPRESS;Initial Catalog=GerenciadorTarefas;Integrated Security=True;Pooling=False;Encrypt=False;TrustServerCertificate=False;";
+        //NoteBook - DESKTOP-V672319\SQLEXPRESS
+        //PCDESKTOP-10DDISU
     }
 }
