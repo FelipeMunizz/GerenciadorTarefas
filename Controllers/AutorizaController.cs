1a8d68af9e9d6ffd75d2e5a8381381b444df82ae
Felipe Muniz <lipe.baterra@gmail.com>
2023-04-23 12:51:03 -0300

    Implementação no controller Autoriza para endpoint redefinir senha

--- Controllers/AutorizaController.cs
+++ Controllers/AutorizaController.cs
@@ -47,41 +47,7 @@ public class AutorizaController : ControllerBase
     [HttpPut("RedefinirSenha")]
     public async Task<IActionResult> RedefinirSenha([FromBody] RedefinirSenhaDTO redefinirSenha)
     {
-        string novaSenha = "";
-        string query = "select ID_USUARIO from USUARIOS where EMAIL = @Email and USUARIO = @Usuario";
-        using (SqlConnection connection = new SqlConnection(AppDbContext.GetConnectionString()))
-        {
-            SqlCommand command = new SqlCommand(query, connection);
-
-            command.Parameters.AddWithValue("@Email", redefinirSenha.Email);
-            command.Parameters.AddWithValue("@Usuario", redefinirSenha.Usuario);
-
-            await connection.OpenAsync();
-            var reader = await command.ExecuteReaderAsync();
-
-            if (!reader.HasRows)
-            {
-                await connection.CloseAsync();
-                return Unauthorized("Usuario não encontrado.");
-            }
-
-            novaSenha = SenhaHelpers.GenerateRandomPassword();
-            var senhaCriptografada = SenhaHelpers.CriptografarSenha(novaSenha);
-
-            bool senhaAtualizada = SenhaHelpers.AtualizarSenha(senhaCriptografada, redefinirSenha.Usuario, redefinirSenha.Email);
-
-            if (!senhaAtualizada)
-            {
-                return NotFound("Não foi possivel gerar uma nova senha");
-            }
-
-            string assunto = "Redefinição de Senha";
-            string mensagem = $"Sua nova senha é: {novaSenha}";
-            bool emailEnviado = _emailHelpers.Enviar(redefinirSenha.Email, assunto, mensagem);
-
-            if (!emailEnviado)
-                return StatusCode(500, "Não foi possível enviar o email com a nova senha");
-        }
+        await _autoriza.RedefinirSenha(redefinirSenha);
 
         return Ok("Sua senha foi atualizada e enviada para o seu email. Tambem verifique sua caixa de spam");
     }
